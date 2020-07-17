715b18cad4b1aa30494f59d1a388753ea6954580
Luudan Matcuoi <ldmcldmc@users.noreply.github.com>
2020-07-17 09:49:36 +0700

    suýt cháy nhà

--- patch text.py
+++ patch text.py
@@ -1,4 +1,7 @@
-import re
+# # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import re, sys
+sys.stdout.reconfigure(encoding='utf-8')
 def findch(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 #Lay list file
@@ -9,24 +12,21 @@ listfile = [f for f in listfile if 'SEEN' in f ]
 for i in range(len(listfile)):
 	listfile[i]=listfile[i][4:-4]
 # listfile=['0414']
-
+patchrldev='cd c: ;yes | cp -rf /cygdrive/z/CLANNAD/Seen.txt Seen.txt;'
 bodem=0
 bodem1=[]
 bodem2=[]
-
-# lay bang ma
 a=[]
 b=[]
-	# Bang ma nguon so sanh
+	# Bảng mã nguồn để so sánh
 ab=['đ','ó','ò','ỏ','õ','ọ','ô','ố','ồ','ổ','ỗ','ộ','ơ','ớ','ờ','ở','ỡ','ợ','á','à','ả','ã','ạ','â','ấ','ầ','ẩ','ẫ','ậ','ă','ắ','ằ','ẳ','ẵ','ặ','ê','ế','ề','ể','ễ','ệ','é','è','ẻ','ẽ','ẹ','ú','ù','ủ','ũ','ụ','ư','ứ','ừ','ử','ữ','ự','í','ì','ỉ','ĩ','ị','ý','ỳ','ỷ','ỹ','ỵ','Đ','Ó','Ò','Ỏ','Õ','Ọ','Ô','Ố','Ồ','Ổ','Ỗ','Ộ','Ơ','Ớ','Ờ','Ở','Ỡ','Ợ','Á','À','Ả','Ã','Ạ','Â','Ấ','Ầ','Ẩ','Ẫ','Ậ','Ă','Ắ','Ằ','Ẳ','Ẵ','Ặ','Ê','Ế','Ề','Ể','Ễ','Ệ','É','È','Ẻ','Ẽ','Ẹ','Ú','Ù','Ủ','Ũ','Ụ','Ư','Ứ','Ừ','Ử','Ữ','Ự','Í','Ì','Ỉ','Ĩ','Ị','Ý','Ỳ','Ỷ','Ỹ','Ỵ']
-	#Bang ma khong dau
+	#Bảng mã không dấu
 bb=["d","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","a","e","e","e","e","e","e","e","e","e","e","e","u","u","u","u","u","u","u","u","u","u","u"," "," "," "," ","i","y","y","y","y","y","D","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","O","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","A","E","E","E","E","E","E","E","E","E","E","E","U","U","U","U","U","U","U","U","U","U","U","I","I","I","I","I","Y","Y","Y","Y","Y"]
-	# Bang ma cho headername
+	# Bảng mã cho headername (tên người nói trong script)
 f=open('bangma cho headername.txt','r',encoding="utf-16")
 bd=f.read().split('\n')
 f.close()
-
-# Bang ma chuan
+# Lấy Bảng mã chuẩn
 f=open('bangma.txt','r',encoding="utf-16")
 lines=f.read().split('\n')
 for t in lines:
@@ -36,16 +36,14 @@ for t in lines:
 	except:
 		pass
 f.close()
-	# danganronpa word :)))
+# Lấy dangopedia word :))) ( chứa các từ dangopedia được dịch )
 f=open('dangoword.txt','r',encoding="utf-16")
 dangoword=f.read().split('\n')
+dangoword = sorted(dangoword, key=len,reverse=True)
 f.close()
+# ------------------- Hết phần thiết lập các giá trị mặc định -----------------------------
 
-
-
-
-
-patchrldev='cd c: ;yes | cp -rf /cygdrive/z/CLANNAD/Seen.txt Seen.txt;'
+# listfile=['3418']
 
 for indexlistfile in range(len(listfile)):
 	done=True
@@ -56,71 +54,52 @@ for indexlistfile in range(len(listfile)):
 	f.close()
 	f=open('seenviet\\SEEN'+listfile[indexlistfile]+'.txt','r',encoding="utf-16")
 	data=f.read().split('\n')
-	dataviet=[]
-	for t in data:
-		if len(t)==0: continue
-		if re.match("<[0-9][0-9][0-9][0-9]>", t[:6]) is not None:
-			dataviet=dataviet+[t]
+	dataviet=[]				# Các line đã dịch
+	dataviet = [ t for t in data if re.match("^<[0-9][0-9][0-9][0-9]>", t) is not None]
 	f.close()
 	f=open('seens\\SEEN'+listfile[indexlistfile]+' - Copy.org','r',encoding="utf-8")
-	dataorg=f.read().split('\n')
+	dataorg=f.read().split('\n') # Các line trong file .org ( file script chỉ cách hiển thị )
+	f.close()
+	f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','w',encoding="utf-8")
+	f.write('\n'.join(dataorg)) # Reset file .org
 	f.close()
 
-	#xet su trung nhau 2 file
+	#  Xem bản HD và dịch có hợp nhau không. Nếu không, skip file đó
 	datarawline=[]
-	for t in dataraw:
-		if re.match("<[0-9][0-9][0-9][0-9]>", t[:6]) is not None:
-			datarawline+=[t[:6]]
+	datarawline = [ t[:6] for t in dataraw if re.match("^<[0-9][0-9][0-9][0-9]>", t) is not None]
 	if not datarawline==[t[:6] for t in dataviet]:
 		done=False
-		print("!!!LOI!!!: File "+listfile[indexlistfile]+' khong trung line   so line: HD:'+str(len(datarawline))+', viet:'+str(len(dataviet)))
+		print("!!!LỖI!!!: File {} không trùng line giữa 2 bản HD: {} và việt: {}, số line lệch là: {}".format(listfile[indexlistfile],str(len(datarawline)),str(len(dataviet)),str(abs(len(dataviet)-len(datarawline)))))
 		continue
 
-	# Tim cac line dangopedia
+	# Lấy các line chứa dangopedia trong file .org
 	dangoline=[]
-	dangow=[]
 	for i in range(len(dataorg)):
 		if "farcall_with(9820, 3" in dataorg[i]:
-			dangow+=[dataorg[i]]
-			dangoline+=[dataorg[i-1][4:]]
+			dangoline+=[[dataorg[i-1][4:],dataorg[i][23:-2]]] # Mảng 2 chiều [so line trong utf, từ dangopedia trong file org]
 
-	# Tim cac line select
+	# Tìm các line select trong file .org (lựa chọn trong khi chơi)
 	indexorg=[]
-	org=[]
+	select_org=[]
 	for t in dataorg:
 		if 'select(' in t: 
 			g=re.findall( r'\#res\<[0-9][0-9][0-9][0-9]\>', t)
 			for i in range(len(g)):
 				g[i]=g[i][5:-1]
-			indexorg=indexorg+[dataorg.index(t)]
-			org=org+[g]
+			select_org=select_org+[g] 		# Mảng 2 chiều, chứa các line select
 
-	def xulyselect(st,dong):
+	def xulyselect(selectline):
+		# Lay cac lua chon cua ham select
 		cacluachon=[]
-		for i in range(len(org)):
-			if st == org[i][0]:
-				# Lay cac lua chon cua ham select
-				for t in org[i]:
-					for ib in range(len(dataviet)):
-						try:
-							if t == dataviet[ib][1:5]:
-								cacluachon=cacluachon+[dataviet[ib]]
-						except:
-							pass
-				# Them dong <9xxx> vao file org
-				if not dataorg[indexorg[i]-1]=='#res<9'+st[1:]+'>':
-					indexorg[i]=indexorg[i]+i
-					dataorg.insert(indexorg[i],'#res<9'+st[1:]+'>')
-
-		# Viet vao file org
-		f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','w',encoding="utf-8")
-		writeorg='\n'.join(dataorg)
-		writeorg = re.sub(r'farcall_with\(9820, 3, \'[A-Za-z ]+\'\)', "farcall_with(9820, 3, '')", writeorg)
-		f.write(writeorg)
-		f.close()
-
-		# Viet cac lua chon vao <9xxx>
-		writedau='<9'+st[1:]+r'> '
+		for t in select_org[[bla[0] for bla in select_org].index(selectline)]:
+			for ib in range(len(dataviet)):
+				try:
+					if t == dataviet[ib][1:5]:
+						cacluachon=cacluachon+[dataviet[ib]]
+				except:
+					pass
+		# Viết các lựa chọn ảo <9xxx> trong file .utf (Kết quả trả về giá trị của hàm)
+		writedau='<9'+selectline[1:]+'> '
 		for t in cacluachon:
 			c=t[7:]
 			if len(c)<40:
@@ -129,27 +108,35 @@ for indexlistfile in range(len(listfile)):
 				c=c.replace(a[i],b[i])
 			writedau=writedau+c+r'\n'
 		writedau=writedau[:-2]+r'\mvy{-'+str(46*(len(cacluachon)+1))+'}\\r\\r'
-
-		# Viet dau cach vao cac lua chon thuc
+		# Viết lại các line chứa lựa chọn thực (Kết quả đổ vào biến dataviet)
 		for t in cacluachon:
-			the=t[:8]+" "*(len(t)-7)
+			the=t[:9]+" "*(int(t[1:5])%5)
 			for i in range(len(ab)):
-				the=the.replace(ab[i],bb[i])
+				the=the.replace(ab[i],bb[i])			# Các lựa chọn thực cần viết không dấu
 			dataviet[dataviet.index(t)]=the
+		# Xử lý file org ( thêm #res<9xxx> để hiển thị các lựa chọn ảo)
+		f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','r',encoding="utf-8")
+		temporg=f.read().split('\n')
+		f.close()
+		for i in range(len(temporg)): 							# Lấy index dòng chứa select(#res<xxxx>, #res<xxxx>)
+			if 'select(#res<'+selectline+'>,' in temporg[i]: 
+				idong = i
+		temporg.insert(idong,'#res<9'+selectline[1:]+'>')
+		writeorg='\n'.join(temporg)
+		f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','w',encoding="utf-8")
+		f.write(writeorg)
+		f.close()
 		return writedau
 
-
-	#Main
-	# so sanh va the chu
 	write=[]
 	for t in dataraw:
-		# skip line trong khong
+		# skip các line trống không
 		if len(t)==0: 
 			write=write+['']
 			continue
 
 		if re.match(r"<[0-9][0-9][0-9][0-9]>", t[:6]) is not None:
-			# test xem co phai ngay thang hay khong -->Format: April 26 (Sat.)
+			# Test xem có phải ngày tháng không -->Format: April 26 (Sat.)
 			for g in dataviet: 
 				if t[:6] in g: x=g
 			if re.search(r"[a-zA-z]+ [0-9]+ \([a-zA-z]{3}\.\)", t):
@@ -158,29 +145,56 @@ for indexlistfile in range(len(listfile)):
 				write=write+[x]
 				continue
 
-			# test xem co phai select khong
-			if t[1:5] in [ org[i][0] for i in range(len(org))]:
-				write=write+[xulyselect(t[1:5],t)]
+			# Test xem có phải lựa chọn không
+			if t[1:5] in [ select_org[i][0] for i in range(len(select_org))]:
+				write=write+[xulyselect(t[1:5])]
 				for g in dataviet: 
 					if t[:6] in g: x=g
 				write=write+[x]
 				continue
-
-			# Tim dong tu dataviet thay vao x
+			# Thế lại bản dịch vào x (do dataviet đã thay đổi)
 			for g in dataviet: 
 				if t[:6] in g: x=g
 
-			# Bo tag \size = Bo chu to nho
-			x=re.sub(r"\\size{[^\!]+}", "", x)
+			# Xử lý dangopedia
+			if t[:6] in [bla[0] for bla in dangoline]:
+				idango=[bla[0] for bla in dangoline].index(t[:6])
+				# Xét xem có tag \g , \c chưa
+				if not ('\\g' in x or '\\c' in x):
+					# Nếu dịch có từ trong dangoword hoặc trong file org thì đặt tag g cho nó
+					if any(k in x for k in dangoword+[dangoline[idango][1]]):
+						for dan in (dangoword+[dangoline[idango][1]]):
+							if not ('\\g' in x or '\\c{intG[1806]}' in x):
+								x=x.replace(dan,'\\g{'+dan+r'}={tag danh tu dong}')
+					else:
+						# Nếu không có thì chép từ file org sang
+						x=x+'\\g{'+dangoline[idango][1]+r'}={tag danh tu dong}'
+				# Nếu cuối dòng có dấu cách và dòng sau đó không có dấu cách thì bảo vệ nó
+				if x[-1]==' ' and dataviet[[p[:6] for p in dataviet].index(t[:6])+1][6:9] != ' \\ ':
+					x=x[:-1]+"\\ \\"
+				# Xóa tất cả từ dangopedia trong file org
+				f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','r',encoding="utf-8")
+				temporg=f.read().split('\n')
+				for bla in range(len(temporg)):
+					if re.search(r'^farcall_with\(9820, 3, \'[A-Za-z ]+\'\)$', temporg[bla]):
+						temporg[bla]="farcall_with(9820, 3, '')"
+				writeorg='\n'.join(temporg)
+				f=open('seens\\SEEN'+listfile[indexlistfile]+'.org','w',encoding="utf-8")
+				f.write(writeorg)
+				f.close()
+
+			# Bỏ tag \size = Bỏ chữ to nhỏ
+			x=re.sub(r"\\size{[^\!]{0,11}}", "", x)
+			print(listfile[indexlistfile]+'\t'+x)
 
 			# Patch tagname / headername
 			m= re.findall( r'\\{[^\{]+}', x)
 			if len(m)!=0:
-				m=m[0][2:-1]
+				m=m[0][2:-1]					# Chứa tag name
 				if any(word in m for word in ab):
 					lch=len(m)
 					if lch<9: 
-						mvx=int(27-7.5*(lch-1))
+						mvx=int(27-7.5*(lch-1))	# Tính căn lề trái
 					else:
 						if lch%2==0: 
 							mvx=int(27-8*8)
@@ -191,17 +205,8 @@ for indexlistfile in range(len(listfile)):
 						mv=mv.replace(ab[im],bd[im])
 					x=x.replace('\\{'+m+'}','\\{'+' '*(lch)+'}'+'\\mv{'+str(mvx)+', -78}\\size{30}'+mv+'\\size{}\\mvx{'+str(0-mvx)+'}\\mvy{32}\\n')
 
-			# Boi mau cho tag \g
-			x=re.sub(r"\\g{([^\{]+)}={[^\{]+}", "zzzazzgzzc\{intG[1806]}\\1zzzazzgzzc{}", x)
-			x=x.replace('zzzazzgzz','\\')
-			# Xu ly dangopedia
-			if t[:6] in dangoline:
-				for word in dangoword:
-					x=x.replace(word,r'\c{intG[1806]}'+word+r'\c{}')
-				if r'\c{intG[1806]}' not in x:
-					# print(listfile[indexlistfile]+'\t'+dangow[dangoline.index(t[:6])]+'\t'+x)
-					word = dangow[dangoline.index(t[:6])].replace("farcall_with(9820, 3, '","")[:-2]
-					x=x+r'\c{intG[1806]}'+word+r'\c{}'
+			# Hightlight cho tag \g
+			x=re.sub(r"\\g{([^}]+)}={[^}]+}", r"\\c{intG[1806]}\1\\c{}", x)
 
 			# Chuyen dau theo rulset vao x
 			for i in range(len(a)):
@@ -221,6 +226,7 @@ for indexlistfile in range(len(listfile)):
 	if done:
 		patchrldev+=' yes | cp -rf /cygdrive/z/CLANNAD/seens/SEEN'+listfile[indexlistfile]+r'.utf SEEN'+listfile[indexlistfile]+r'.utf; yes | cp -rf /cygdrive/z/CLANNAD/seens/SEEN'+listfile[indexlistfile]+r'.org SEEN'+listfile[indexlistfile]+r'.org;rlc -i /cygdrive/z/CLANNAD/GAMEEXEbbb.ini -f 1.5 -e utf8 SEEN'+listfile[indexlistfile]+r'.org; kprl -k Seen.txt '+listfile[indexlistfile]+r'; kprl -a Seen.txt SEEN'+listfile[indexlistfile]+r'.TXT;'
 	patchrldev+='rm /cygdrive/z/CLANNAD/Seen.txt; yes | cp -rf Seen.txt /cygdrive/z/CLANNAD;'
+
 print(patchrldev)
 
 #copy lenh patch rledv vao clipboard (Neu da cai pyperclip) 
@@ -228,4 +234,4 @@ try:
 	import pyperclip
 	pyperclip.copy(patchrldev)
 except:
-	pass
+	print('pyperclip not installed')
